e261ecec6698602b72a1142886d2d565859c3087
EliTE08 <memeinsta84@gmail.com>
2024-03-06 15:46:54 -0500

    Added Movement and Camera Follow
    I added basic AD movement, W for jump, and space to flip gravity. Also, I used Cinemachine to follow player smoothly.

--- GravityGame/Assets/Scripts/PlayerController.cs
+++ GravityGame/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-    [SerializeField] float speed;
-    [SerializeField] float jumpForce;
+    [SerializeField] float speed; // 250
+    [SerializeField] float jumpForce; // 300
+    
+    [SerializeField] Vector3 boxSize;
+    [SerializeField] float maxDistance;
+    [SerializeField] LayerMask layerMask;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Application.targetFrameRate = 60;
     }
 
     void Update()
     {
+        float vert = Input.GetAxisRaw("Vertical");
+        Debug.Log(vert);
+        Move();
+        if(vert != 0 && isGrounded()){
+            Jump();
+        }
+        if(Input.GetButtonDown("Jump")){
+            Flip();
+        }
+    }
+
+    private void Move(){
+        
         float horiz = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horiz*speed*Time.deltaTime, rb.velocity.y);
-        Debug.Log(horiz);
 
-        if(Input.GetButtonDown("Jump")){
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce*Time.deltaTime);
+    }
+
+    private void Jump(){
+
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce*Time.deltaTime);
+
+    }
+
+    private void Flip(){
+        
+        rb.gravityScale = -rb.gravityScale;
+
+    }
+
+    public bool isGrounded(){
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up,maxDistance,layerMask)){
+            return true;
         }
+        else{
+            return false;
+        }
+    }
+
+
+
+    void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position-transform.up*maxDistance,boxSize);
     }
 }
